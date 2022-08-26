92616b1dfae3e2e705e85adaaf68d6939477884f
MNMercan <numan.mercan24@gmail.com>
2022-08-26 11:06:43 +0300

    added populer city from apı

--- script.js
+++ script.js
@@ -3,31 +3,82 @@ const key = 'a6f77a37693e3437023f21e2718e1066';
 const iconUrl = 'https://openweathermap.org/img/wn/'
 const searchBar = document.querySelector('#searchBar');
 
-const codeRun = () => {
-  const london = getResult("london");
-  const istanbul = getResult("istanbul");
-  const ny = getResult("new york")
-  const berlin = getResult("berlin");
-  let londonTemp = document.querySelector('#london-temp');
-  londonTemp.innerText = `${Math.round(londonTemp.main.temp)}°C`;
-}
 
-window.onload = codeRun;
 
 const setQuery = (e) => {
-    if(e.keyCode == "13"){
+    if (e.keyCode == "13") {
         getResult(searchBar.value);
         searchBar.value = "";
     }
 }
 
+const popular = () => {
+    getResult2('london');
+    getResult2('istanbul');
+    getResult2('new york');
+    getResult2('berlin');
+}
+const getResult2 = (cityName) => {
+    let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`;
+    fetch(query)
+        .then(weather => {
+            return weather.json();
+        })
+        .then(displayResult2)
+}
+
+const displayResult2 = (result) => {
+
+    const cityName = result.name;
+    console.log(cityName)
+    switch (cityName) {
+        case 'Londra':
+            let londonTemp = document.querySelector("#london-temp");
+            londonTemp.innerText = `${Math.round(result.main.temp)}°C`;
+            let londonDesc = document.querySelector('#london-desc');
+            londonDesc.innerText = `${result.weather[0].description}`;
+            let londonIcon = document.querySelector('#london-img');
+            londonIcon.src = `${iconUrl}${result.weather[0].icon}.png`
+            break;
+        case 'New York kenti':
+            let nyTemp = document.querySelector("#ny-temp");
+            nyTemp.innerText = `${Math.round(result.main.temp)}°C`;
+            let nyDesc = document.querySelector('#ny-desc');
+            nyDesc.innerText = `${result.weather[0].description}`;
+            let nyIcon = document.querySelector('#ny-img');
+            nyIcon.src = `${iconUrl}${result.weather[0].icon}.png`
+            break;
+        case 'İstanbul':
+            let istTemp = document.querySelector("#istanbul-temp");
+            istTemp.innerText = `${Math.round(result.main.temp)}°C`;
+            let istDesc = document.querySelector('#ist-desc');
+            istDesc.innerText = `${result.weather[0].description}`;
+            let istIcon = document.querySelector('#ist-img');
+            istIcon.src = `${iconUrl}${result.weather[0].icon}.png`
+            break;
+        case 'Berlin':
+
+            let berlinTemp = document.querySelector("#berlin-temp");
+            berlinTemp.innerText = `${Math.round(result.main.temp)}°C`;
+            let berlinDesc = document.querySelector('#berlin-desc');
+            berlinDesc.innerText = `${result.weather[0].description}`;
+            let berlinIcon = document.querySelector('#berlin-img');
+            berlinIcon.src = `${iconUrl}${result.weather[0].icon}.png`
+            break;
+        default:
+            console.log("Hata var");
+            break;
+    }
+}
+
+
 const getResult = (cityName) => {
     let query = `${url}weather?q=${cityName}&appid=${key}&units=metric&lang=tr`;
     fetch(query)
-    .then(weather => {
-        return weather.json();
-    })
-    .then(displayResult)
+        .then(weather => {
+            return weather.json();
+        })
+        .then(displayResult)
 }
 
 const displayResult = (result) => {
@@ -50,7 +101,8 @@ const displayResult = (result) => {
     let humidity = document.querySelector('.humidity');
     humidity.innerText = `Nem : ${result.main.humidity}`
 
-    
+
 }
 
+window.onload = (popular);
 searchBar.addEventListener('keypress', setQuery);
